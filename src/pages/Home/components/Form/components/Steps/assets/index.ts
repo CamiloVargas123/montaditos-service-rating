9309a94d65814f8b4ac042d32dd0330946932854
Camilo Vargas <camilovargasingm@gmail.com>
2023-06-15 23:52:16 -0500

    Feat: Add custom icons component

---
+++ src/pages/Home/components/Form/components/Steps/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as IconRate1 } from './IconRate1';
+export { default as IconRate2 } from './IconRate2';
+export { default as IconRate3 } from './IconRate3';
+export { default as IconRate4 } from './IconRate4';
+export { default as IconRate5 } from './IconRate5';
