67342c7be300564bf88f2d3f202bfc3df6d3a626
Camilo Vargas <camilovargasingm@gmail.com>
2023-06-16 23:29:04 -0500

    Feat: Add text in step diferent to one

--- src/pages/Home/components/Footer/Footer.tsx
+++ src/pages/Home/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import { LocationOn, Phone } from '@mui/icons-material';
 import { Stack, Typography, styled } from "@mui/material";
+import { useContext } from 'react';
+import { StepContext } from '../../provider';
 
 const ImageOverlayContainer = styled('div')(() => ({
   position: 'relative',
   width: '100%',
-  height: '18vh',
+  height: '30vh',
   backgroundImage: 'url(https://fotografias.larazon.es/clipping/cmsimages02/2022/09/05/32132660-464F-445D-B600-76932DF94D5F/98.jpg?crop=1600,900,x0,y0&width=1900&height=1069&optimize=low&format=webply)',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
@@ -12,7 +14,7 @@ const ImageOverlayContainer = styled('div')(() => ({
 
 const Overlay = styled(Stack)(() => ({
   flexDirection: "row",
-  padding: "5px 20px",
+  padding: "20px",
   color: "white",
   position: 'absolute',
   top: 0,
@@ -35,25 +37,38 @@ const SeparatedBar = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
 }));
 
-export default function Footer() {
+const TextPromotion = styled(Typography)({
+  textTransform: "uppercase",
+  fontSize: "1.4rem",
+  maxWidth: "200px",
+  fontWeight: 700,
+  textShadow: '0px 4px 4px rgba(0, 0, 0, 1)'
+})
 
+export default function Footer() {
+  const { currentStep } = useContext(StepContext)
   return (
     <ImageOverlayContainer as={"footer"}>
       <Overlay>
         <SeparatedBar />
-        <Stack alignItems={"center"}>
-          <Stack direction={"row"} spacing={2}>
-            <Stack direction={"row"} alignItems={"center"}>
-              <Phone fontSize="medium" />
-              <Typography fontWeight={"bold"} fontSize={"20px"}>918 654 259</Typography>
+        {
+          currentStep === 0 ?
+            <Stack alignItems={"center"}>
+              <Stack direction={"row"} spacing={2}>
+                <Stack direction={"row"} alignItems={"center"}>
+                  <Phone fontSize="medium" />
+                  <Typography fontWeight={"bold"} fontSize={"20px"}>918 654 259</Typography>
+                </Stack>
+                <Stack direction={"row"} alignItems={"center"}>
+                  <Phone fontSize="medium" />
+                  <Typography fontWeight={"bold"} fontSize={"20px"}>623 437706</Typography>
+                </Stack>
+              </Stack>
+              <Typography fontWeight={"bold"} fontSize={"20px"}>www.spain.100montaditos.com/es</Typography>
             </Stack>
-            <Stack direction={"row"} alignItems={"center"}>
-              <Phone fontSize="medium" />
-              <Typography fontWeight={"bold"} fontSize={"20px"}>623 437706</Typography>
-            </Stack>
-          </Stack>
-          <Typography fontWeight={"bold"} fontSize={"20px"}>www.spain.100montaditos.com/es</Typography>
-        </Stack>
+            :
+            <TextPromotion>promoción en montaditos los jueves</TextPromotion>
+        }
         <Stack alignItems={"center"} spacing={1}>
           <LocationOn fontSize="large" />
           <Typography color={"black"} fontWeight={"bold"} padding={"2px"} bgcolor={"white"} borderRadius={1}>Centro Comercial Manoteras</Typography>
